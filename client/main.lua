87d407bbb6b68c5d5138fe873feaf97c5031b973
xThrasherrr <101474430+xThrasherrr@users.noreply.github.com>
2022-09-27 21:42:12 -0600

    Target

--- client/main.lua
+++ client/main.lua
@@ -1,23 +1,24 @@
 local PSRCore = exports['psr-core']:GetCoreObject()
 local blips = {}
+local peds = {}
 local banks
 
-Citizen.CreateThread(function()
-    for banks, v in pairs(Config.BankLocations) do
-        exports['psr-core']:createPrompt(v.bankname, v.coords, 0xF3830D8E, 'Open ' .. v.name, {
-            type = 'client',
-            event = 'psr-banking:openBankScreen',
-            args = { },
-        })
-        if v.showblip == true then
-            local BankBlip = N_0x554d9d53f696d002(1664425300, v.coords)
-            SetBlipSprite(BankBlip, Config.Blip.blipType, 1)
-            SetBlipScale(BankBlip, Config.Blip.blipScale)
-        end
-    end
-end)
+-- Citizen.CreateThread(function()
+--     for banks, v in pairs(Config.BankLocations) do
+--         exports['psr-core']:createPrompt(v.bankname, v.coords, 0xF3830D8E, 'Open ' .. v.name, {
+--             type = 'client',
+--             event = 'psr-banking:openBankScreen',
+--             args = { },
+--         })
+--         if v.showblip == true then
+--             local BankBlip = N_0x554d9d53f696d002(1664425300, v.coords)
+--             SetBlipSprite(BankBlip, Config.Blip.blipType, 1)
+--             SetBlipScale(BankBlip, Config.Blip.blipScale)
+--         end
+--     end
+-- end)
 
-Citizen.CreateThread(function()
+CreateThread(function()
     for k,v in pairs(Config.BankDoors) do
         --for v, door in pairs(k) do
         Citizen.InvokeNative(0xD99229FE93B46286,v,1,1,0,0,0,0)
@@ -182,4 +183,95 @@ RegisterNUICallback("updatePin", function(data, cb)
         cb("ok")
     end
     cb(nil)
+end)
+
+---------------------------------------------------
+-------------- TARGET ZONES & BLIPS ---------------
+---------------------------------------------------
+RegisterNetEvent('psr-banking:StartUp', function()
+    for k, v in pairs(Config.BankLocations) do
+
+        -- BLIPS --
+        if v.showblip == true then
+            StoreBlip = N_0x554d9d53f696d002(1664425300, v.coords)
+            SetBlipSprite(StoreBlip, -2128054417, 52)
+            SetBlipScale(StoreBlip, 0.2)
+        end
+        table.insert(blips, StoreBlip)
+
+        while not HasModelLoaded( GetHashKey(Config.BankLocations[k].model) ) do
+            Wait(500)
+            RequestModel( GetHashKey(Config.BankLocations[k].model) )
+        end
+        local npc = CreatePed(GetHashKey(Config.BankLocations[k].model), Config.BankLocations[k].coords.x, Config.BankLocations[k].coords.y, Config.BankLocations[k].coords.z - 1, Config.BankLocations[k].heading, false, false, 0, 0)
+        while not DoesEntityExist(npc) do
+            Wait(300)
+        end
+        Citizen.InvokeNative(0x283978A15512B2FE, npc, true)
+        FreezeEntityPosition(npc, false)
+        SetEntityInvincible(npc, true)
+        TaskStandStill(npc, -1)
+        Wait(100)
+        SetPedRelationshipGroupHash(npc, GetHashKey(Config.BankLocations[k].model))
+        SetEntityCanBeDamagedByRelationshipGroup(npc, false, `PLAYER`)
+        SetEntityAsMissionEntity(npc, true, true)
+        SetModelAsNoLongerNeeded(GetHashKey(Config.BankLocations[k].model))
+
+        -- TARGET LOCATIONS --
+        exports['psr-target']:AddCircleZone(v.name, v.coords, 1.0, { -- The name has to be unique, the coords a vector3 as shown and the 1.5 is the radius which has to be a float value
+            name = v.name, -- This is the name of the zone recognized by PolyZone, this has to be unique so it doesn't mess up with other zones
+            debugPoly = Config.Debug, -- This is for enabling/disabling the drawing of the box, it accepts only a boolean value (true or false), when true it will draw the polyzone in green
+        }, {
+            options = { -- This is your options table, in this table all the options will be specified for the target to accept
+                {
+                    type = "client",
+                    event = "psr-banking:openBankScreen",
+                    icon = 'fas fa-money-bill',
+                    label = 'Banking'
+                }
+            },
+            distance = 2.5, -- This is the distance for you to be at for the target to turn blue, this is in GTA units and has to be a float value
+        })
+
+        table.insert(peds, npc)
+    end
+end)
+
+-------------------------------------------
+---------- RESOURCE START / STOP ----------
+-------------------------------------------
+AddEventHandler('PSRCore:Client:OnPlayerLoaded', function()
+    TriggerEvent('psr-banking:StartUp') -- Zones and Blips
+end)
+
+AddEventHandler('PSRCore:Client:OnPlayerUnloaded', function()
+    for k, v in pairs(Config.BankLocations) do
+        exports['psr-target']:RemoveZone(v.name) -- Remove Zones for script restarts
+    end
+    for k,v in pairs(blips) do
+        RemoveBlip(blips[k]) -- Remove Blips for script restarts
+    end
+    for _,v in pairs(peds) do
+        DeletePed(v)
+    end
+end)
+
+AddEventHandler('onResourceStart', function(resourceName)
+    if GetCurrentResourceName() == resourceName then
+        TriggerEvent('psr-banking:StartUp') -- Zones and Blips
+    end
+end)
+
+AddEventHandler('onResourceStop', function(resourceName)
+    if GetCurrentResourceName() == resourceName then
+        for k, v in pairs(Config.BankLocations) do
+            exports['psr-target']:RemoveZone(v.name) -- Remove Zones for script restarts
+        end
+        for k,v in pairs(blips) do
+            RemoveBlip(blips[k]) -- Remove Blips for script restarts
+        end
+        for _,v in pairs(peds) do
+            DeletePed(v)
+        end
+    end
 end)
--- config/config.lua
+++ config/config.lua
@@ -1,13 +1,14 @@
 Config = {}
-
-Config.cardTypes = { "bankbook"}
+Config.Debug = false -- Enable to debug PolyZones
 
 Config.BankLocations = {
-    {name = 'Bank of Rhodes',           bankname = "rhodes-bank",		coords = vector3(1292.307, -1301.539, 77.04012),    showblip = true},
-    {name = 'Lemoyne National Bank',    bankname = "lemoyne-bank",		coords = vector3(2644.579, -1292.313, 52.24956),    showblip = true},
-    {name = 'Valentine Savings Bank',   bankname = "valentine-bank",	coords = vector3(-308.4189, 775.8842, 118.7017),    showblip = true},
-    {name = 'First National Bank',      bankname = "city-bank",			coords = vector3(-813.1633, -1277.486, 43.63771),   showblip = true},
+    [1] = {name = 'Bank of Rhodes',           model = "S_M_M_BankClerk_01",		bankType = "BANKS",     	coords = vector3(1292.81, -1304.62, 77.04),    		heading = 325.62,		showblip = true},
+    [2] = {name = 'Lemoyne National Bank',    model = "S_M_M_BankClerk_01",		bankType = "BANKS2",    	coords = vector3(2644.9, -1293.99, 52.25),   		heading = 18.57,		showblip = true},
+    [3] = {name = 'Valentine Savings Bank',   model = "S_M_M_BankClerk_01",		bankType = "BANKS3",    	coords = vector3(-308.06, 773.64, 118.7), 			heading = 2.56,			showblip = true},
+    [4] = {name = 'First National Bank',      model = "S_M_M_BankClerk_01",		bankType = "BANKS4",    	coords = vector3(-811.96, -1275.43, 43.64),   		heading = 186.83,		showblip = true},
 }
+
+Config.cardTypes = { "bankbook"}
 
 Config.Blip = {
     blipName = Lang:t('info.bank_blip'),
@@ -24,7 +25,7 @@ Config.BankDoors = { --Doors that will always be open unless robbery has started
 	334467483, -- inner door1
 	3718620420, -- inner door2
 	576950805, -- vault door
-	
+
 	-- Bank of Rhodes
 	3317756151, -- main door
 	3088209306, -- main door
@@ -32,7 +33,7 @@ Config.BankDoors = { --Doors that will always be open unless robbery has started
 	3142122679, -- inner door2
 	1634148892, -- inner door3
 	3483244267, -- vault
-  
+
 	-- Lemoyne National Bank Saint Denis
 	2158285782, -- main door
 	1733501235, -- main door
@@ -42,13 +43,13 @@ Config.BankDoors = { --Doors that will always be open unless robbery has started
 	965922748, -- manager door
 	1634115439, -- manager door
 	1751238140, -- vault
-	
+
 	-- West Elizabeth Co-Operative Bank Blackwater
 	531022111, -- main door
 	2117902999, -- inner door
 	2817192481, -- manager door
 	1462330364, -- vault door
-	
+
 	-- Bank of Armadillo
 	3101287960, -- main door
 	3550475905, -- inner door
